98ba0fc78b21dd2ae9245a71877b648c5d27b14f
eciodiniz <60238061+eciodiniz@users.noreply.github.com>
2025-12-15 13:48:08 +0100

    Import additional modules for SCENFIRE framework

--- src/scenfirepy/__init__.py
+++ src/scenfirepy/__init__.py
@@ -5,3 +5,11 @@ Python implementation of the SCENFIRE fire-scenario framework.
 """
 
 __version__ = "0.1.0"
+
+from .io import read_flp20_csv
+from .preprocess import validate_fire_dataframe, compute_fire_size
+from .distribution import fit_powerlaw_distribution, build_target_histogram
+from .selection import calculate_discrepancy, select_events
+from .spatial import validate_geodataframe
+from .raster import rasterize_geometries
+from .viz import plot_fire_size_distribution
