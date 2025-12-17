0169c948caf69845f0589571cb03404778c442f4
eciodiniz <60238061+eciodiniz@users.noreply.github.com>
2025-12-17 05:28:46 +0100

    Update version retrieval to use importlib.metadata

--- src/scenfirepy/__init__.py
+++ src/scenfirepy/__init__.py
@@ -4,7 +4,9 @@ scenfirepy
 Python implementation of the SCENFIRE fire-scenario framework.
 """
 
-__version__ = "0.1.0"
+from importlib.metadata import version
+
+__version__ = version("scenfirepy")
 
 from .preprocess import check_fire_data
 from .distribution import build_target_hist, calculate_discrepancy
